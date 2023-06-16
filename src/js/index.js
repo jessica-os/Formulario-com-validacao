389dc8353ff690089085fa4b93dffb29aad31093
JessicaOliveira <elisa0js@gmail.com>
2023-06-16 20:12:30 -0300

    ajuste js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,24 @@
 const formulario = document.getElementById("form");
 const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio");
+const enviar = document.querySelectorAll(".botao-enviar");
 
-camposObrigatorios.forEach(function (item) {
-	if (item.value == "") {
-		item.classList.add("campo-vazio");
-	} else {
-		item.classList.add("campo-preenchido");
-	}
+formulario.addEventListener("submit", function (event) {
+	event.preventDefault();
+	validarCampoVazio(camposObrigatorios);
 });
+
+function validarCampoVazio(camposObrigatorios) {
+	camposObrigatorios.forEach((item)=> {
+		if (item.value === "") {
+			const controle = item.parentElement;
+			const mensagemErro = controle.querySelector(".span");
+			mensagemErro.classList.add("span-visivel");
+			item.classList.add("campo-vazio");
+			item.classList.remove("campo-preenchido");
+		} else {
+			item.classList.add("campo-preenchido");
+			item.classList.remove("campo-vazio");
+		}
+	});
+}
+
