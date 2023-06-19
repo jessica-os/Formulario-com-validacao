cebd71a6025a34e1b357ed6e083d36dab628398d
JessicaOliveira <elisa0js@gmail.com>
2023-06-19 14:17:24 -0300

    atualizações JS

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,12 @@ formulario.addEventListener("submit", function (event) {
 });
 
 function validarCampoVazio(camposObrigatorios) {
-	camposObrigatorios.forEach((item)=> {
+	camposObrigatorios.forEach(item => {
 		if (item.value === "") {
 			const controle = item.parentElement;
 			const mensagemErro = controle.querySelector(".span");
 			mensagemErro.classList.add("span-visivel");
+
 			item.classList.add("campo-vazio");
 			item.classList.remove("campo-preenchido");
 		} else {
@@ -21,4 +22,11 @@ function validarCampoVazio(camposObrigatorios) {
 		}
 	});
 }
+function somenteNumeros(e) {
+	var code = e.which || e.keyCode;
 
+	if (code < 48 || code > 57) {
+		e.preventDefault();
+		return false;
+	}
+}
